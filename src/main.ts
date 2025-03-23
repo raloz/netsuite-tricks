3aeac1f82ce9a4fab3dd27aeccbcc4ca965e2571
Octavio Quiroz <octavioquiroz30@gmail.com>
2025-03-23 16:10:38 -0600

    feature: adding icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import './style.css';
+import NetSuiteTricksIcon from '../public/icons/icon-128.png';
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>NetSuite Tricks</h1>
+    <img src="${NetSuiteTricksIcon}" style="width:96px;" />
+    <p>
+      NetSuite Tricks 🪄 is a tool for improve your developer experience.
+    </p>
   </div>
 `;
